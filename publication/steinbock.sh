0c7145378cb4c4f0ea7771fa693c11818a0f1055
nilseling <nils.eling@gmx.de>
2023-01-27 10:54:34 +0100

    Moved steinbock script

--- publication/steinbock.sh
+++ publication/steinbock.sh
@@ -6,22 +6,19 @@ cd ${BASEDIR}
 
 # setup steinbock alias
 shopt -s expand_aliases
-alias steinbock="docker run -v ${BASEDIR}:/data -u $(id -u):$(id -g) ghcr.io/bodenmillergroup/steinbock:0.15.0"
+alias steinbock="docker run -v ${BASEDIR}/data/steinbock:/data -u $(id -u):$(id -g) ghcr.io/bodenmillergroup/steinbock:0.15.0"
 
 # image pre-processing
-{ time steinbock preprocess imc images --hpf 50 } 2> steinbock_timing.txt
+{ time steinbock preprocess imc images --hpf 50; } 2> steinbock_timing.txt
 
 # image segmentation
-{ time steinbock segment deepcell --minmax } 2>> steinbock_timing.txt
+{ time steinbock segment deepcell --minmax; } 2>> steinbock_timing.txt
 
 # intensity measurement
-{ time steinbock measure intensities } 2>> steinbock_timing.txt
+{ time steinbock measure intensities; } 2>> steinbock_timing.txt
 
 # regionprops measurement
-{ time steinbock measure regionprops } 2>> steinbock_timing.txt
+{ time steinbock measure regionprops; } 2>> steinbock_timing.txt
 
 # spatial cell graph construction
-{ time steinbock measure neighbors --type expansion --dmax 4 } 2>> steinbock_timing.txt
-
-
-
+{ time steinbock measure neighbors --type expansion --dmax 4; } 2>> steinbock_timing.txt
